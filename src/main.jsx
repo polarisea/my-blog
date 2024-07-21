73d6f9a600b3e74fbd911e0a3af3dee7791e8103
polarisea <vdinhdung2001@gmail.com>
2024-07-21 10:30:35 +0700

    Setup normalize css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import "./styles/normalize.css";
 import "./styles/global.css";
 import "./styles/animations.css";
 
